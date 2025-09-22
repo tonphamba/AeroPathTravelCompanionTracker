7baa3ae0950812c93ce14bd1e58025e502ef1d4f
tonphamba <ton.phamba@icloud.com>
2025-09-23 06:20:48 +0700

    Delete Dependencies.swift

--- Dependencies.swift
+++
@@ -1,8 +0,0 @@
-import ProjectDescription
-
-let dependencies = Dependencies(
-    swiftPackageManager: .init([
-        // В будущем здесь можно добавить внешние зависимости
-    ]),
-    platforms: [.iOS]
-)
